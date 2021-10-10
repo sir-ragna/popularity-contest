4e6c2035465328959b01d95494ea74ae0add1d8c
ragna <sir.ragna@gmail.com>
2021-10-10 21:43:02 +0200

    Add some section header parsing (seems broken)

--- main.c
+++ main.c
@@ -73,6 +73,34 @@ typedef struct
   uint16_t	e_shstrndx;		/* Section header string table index */
 } Elf64_header;
 
+typedef struct
+{
+  uint32_t	sh_name;		/* Section name (string tbl index) */
+  uint32_t	sh_type;		/* Section type */
+  uint32_t	sh_flags;		/* Section flags */
+  uint32_t	sh_addr;		/* Section virtual addr at execution */
+  uint32_t	sh_offset;		/* Section file offset */
+  uint32_t	sh_size;		/* Section size in bytes */
+  uint32_t	sh_link;		/* Link to another section */
+  uint32_t	sh_info;		/* Additional section information */
+  uint32_t	sh_addralign;		/* Section alignment */
+  uint32_t	sh_entsize;		/* Entry size if section holds table */
+} Elf32_sectionheader;
+
+typedef struct
+{
+  uint32_t	sh_name;		/* Section name (string tbl index) */
+  uint32_t	sh_type;		/* Section type */
+  uint64_t	sh_flags;		/* Section flags */
+  uint64_t	sh_addr;		/* Section virtual addr at execution */
+  uint64_t	sh_offset;		/* Section file offset */
+  uint64_t	sh_size;		/* Section size in bytes */
+  uint32_t	sh_link;		/* Link to another section */
+  uint32_t	sh_info;		/* Additional section information */
+  uint64_t	sh_addralign;		/* Section alignment */
+  uint64_t	sh_entsize;		/* Entry size if section holds table */
+} Elf64_sectionheader;
+
 typedef struct {
     unsigned int counter; // count occurences
     char instr_str[32]; // store mnemonic
@@ -105,7 +133,7 @@ int popularity_contest(const char *filename) {
   }
 
   printf("e_type: %d\n", elfHeader.e_type);
-  return 0;
+  
   //if (elfHeader.e_type)
 
   // printf("Entrypoint %08x %lu\n", (unsigned int)elfHeader.e_entry, elfHeader.e_entry);
@@ -128,9 +156,9 @@ int popularity_contest(const char *filename) {
   Elf64_Shdr *section_headers;
   section_headers = malloc(sizeof(Elf64_Shdr) * elfHeader.e_shnum); 
   
-  // printf("Total file size: %d\n", amount_bytes);
-  // printf("Section Header Offset: %zu\n", elfHeader.e_shoff);
-  // printf("Section header size: %d\n", elfHeader.e_shentsize);
+  printf("Total file size: %d\n", amount_bytes);
+  printf("Section Header Offset: %zu\n", elfHeader.e_shoff);
+  printf("Section header size: %d\n", elfHeader.e_shentsize);
   // printf("Section header struct size: %zu\n", sizeof(Elf64_Shdr));
   ret = fseek(fp, elfHeader.e_shoff, SEEK_SET); 
   if (ret == -1) {
@@ -152,10 +180,10 @@ int popularity_contest(const char *filename) {
     }
     
     // Print the properties of the Section Header
-    printf("%i Str offset: %i, Type: %i, offset: %lu (%#lx), Size: %lu (%#lx)\n",
+    printf("%i Str offset: %i, Type: %i, offset: %lu (%#lx), Size: %lu (%#lx), Section table hold: %d\n",
       i, section_headers[i].sh_name, section_headers[i].sh_type,
       section_headers[i].sh_offset, section_headers[i].sh_offset,
-      section_headers[i].sh_size, section_headers[i].sh_size);
+      section_headers[i].sh_size, section_headers[i].sh_size, section_headers[i].sh_entsize);
 
     if (i == elfHeader.e_shstrndx) { // This is the _Section Header String Table_
       ret = fseek(fp, section_headers[i].sh_offset, SEEK_SET); // set cursor
@@ -339,8 +367,8 @@ int parse_64_bit_header(FILE *fp) {
   }
 
   printf("Entrypoint: %#016x\n", elfheader.e_entry);
-  printf("Program header table offset: %#016x\n", elfheader.e_phoff);
-  printf("Section header table offset: %#016x\n", elfheader.e_shoff);
+  printf("Program header table offset: %d\n", elfheader.e_phoff);
+  printf("Section header table offset: %d\n", elfheader.e_shoff);
   printf("Flags: %#08x\n", elfheader.e_flags);
   printf("ELF Header size: %#04x (%i bytes)\n", elfheader.e_ehsize, elfheader.e_ehsize);
 
@@ -352,7 +380,127 @@ int parse_64_bit_header(FILE *fp) {
 
   printf("Section header table entry count: %i\n", elfheader.e_shnum);
   printf("Section header string table index: %i\n", elfheader.e_shstrndx);
+  
+  printf("Section header size: %d(fixed)\n", elfheader.e_shentsize);
 
+  if (elfheader.e_shentsize != sizeof(Elf64_sectionheader)) {
+    /* This might be a header issue or my the code */
+    puts("Section header size doesn't match Elf64_sectionheader struct");
+    exit(3);
+  }
+
+  /* Set the cursor ready for reading out for the section headers */
+  if (-1 == fseek(fp, elfheader.e_shoff, SEEK_SET)) {
+      fprintf(stderr, "fseek failed (eof=%d)\n", feof(fp));
+      return 1;
+  }
+
+  /* Allocate the required memory */
+  Elf64_sectionheader *section_headers;
+  section_headers = malloc(sizeof(Elf64_sectionheader) * elfheader.e_shnum);
+
+  /* Read out the section headers */
+  for (int i = 0; i < elfheader.e_shnum; i++) {
+    int ret = fread(section_headers + i, sizeof(Elf64_sectionheader), 1, fp);
+    if (ret != 1) {
+      fprintf(stderr, "eof value: %d\n", feof(fp));
+      fprintf(stderr, "ferror value: %d\n", ferror(fp));
+      perror("fread");
+      if (section_headers) {
+        free(section_headers);
+      }
+      return 1;
+    }
+  }
+
+  /* Print ouf the section headers one by one */
+  for (int i = 0; i < elfheader.e_shnum; i++) {
+    Elf64_sectionheader sh = section_headers[i];
+    printf("%d --- tbl index %d\n", i, sh.sh_name);
+    printf("\tType code: %d\n", sh.sh_type);
+    switch (sh.sh_type)
+    {
+    case 0:
+      puts("\tType: NULL section");
+      break;
+    case 1:
+      puts("\tType: Program information");
+      break;
+    case 2:
+      puts("\tType: Symbol table");
+      break;
+    case 3:
+      puts("\tType: String table");
+      break;
+    case 4:
+      puts("\tType: Relocation (no addend)");
+      break;
+    case 5:
+      puts("\tType: Symbol hash table");
+      break;
+    case 6:
+      puts("\tType: Dynamic linking information");
+      break;
+    case 7:
+      puts("\tType: Notes");
+      break;
+    case 8:
+      puts("\tType: Not present in file");
+      break;
+    case 9:
+      puts("\tType: Relocation (with addend)");
+      break;
+    case 10:
+      puts("\tType: Reserved");
+      break;
+    case 11:
+      puts("\tType: Dynamic linker symbol table");
+      break;
+    case 14:
+      puts("\tType: Array of constructors");
+      break;
+    case 15:
+      puts("\tType: Array of destructors");
+      break;
+    case 16:
+      puts("\tType: Array of pre-constructors");
+      break;
+    case 17:
+      puts("\tType: Section group");
+      break;
+    default:
+      puts("\tType is UKNOWN");
+      break;
+    }
+    printf("\tFlags: %d\n", sh.sh_flags);
+    if (sh.sh_flags & 0x1) {
+      puts("\t\tWriteable section");
+    }
+    if (sh.sh_flags & 0x2) {
+      puts("\t\tAllocate in memory");
+    }
+    if (sh.sh_flags & 0x4) {
+      puts("\t\tExecutable");
+    }
+    if (sh.sh_flags & 0x8) {
+      puts("\t\tMerge");
+    }
+    if (sh.sh_flags & 0x16) {
+      puts("\t\tContains nul-terminated strings");
+    }
+    printf("\tVirtual Address: %#016x\n", sh.sh_addr);
+    printf("\tSection file offset: %d\n", sh.sh_offset);
+    printf("\tSection size: %d\n", sh.sh_size);
+    printf("\tLink: %#08x\n", sh.sh_link); /* What is Link? */
+    printf("\tInfo: %#08x\n", sh.sh_info);
+    printf("\tSection alignment: %#016x\n", sh.sh_addralign);
+    printf("\tEntry size: %d\n", sh.sh_entsize);
+    puts("******************************");
+  }
+
+  if (section_headers) {
+    free(section_headers);
+  }
   return 0;
 }
 
@@ -464,7 +612,7 @@ int main(int argc, char *argv[]) {
       if (strcmp("-p", argv[i]) == 0) {
         continue;
       }
-      printf("Parsing file: %s\n", argv[i]);
+      printf("File name: %s\n", argv[i]);
       parse_elf_header(argv[i]);
     }
     return 0;
